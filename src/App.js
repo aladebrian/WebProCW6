1977e616273ed09c8320bbe9d638346f8698345b
Brian <aladebrian@gmail.com>
2025-04-20 17:35:06 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import HelloWorld from "./HelloWorld";
+import Counter from "./Counter";
+import FilteredList from "./FilteredList";
+<link
+    rel="stylesheet"
+    href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+    integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+    crossorigin="anonymous"
+></link>;
+
+const produce = [
+    { name: "Apple", type: "Fruit" },
+    { name: "Pineapple", type: "Fruit" },
+    { name: "Banana", type: "Fruit" },
+    { name: "Pear", type: "Fruit" },
+    { name: "Strawberry", type: "Fruit" },
+    { name: "Orange", type: "Fruit" },
+    { name: "Lettuce", type: "Vegetable" },
+    { name: "Cucumber", type: "Vegetable" },
+    { name: "Eggplant", type: "Vegetable" },
+    { name: "Squash", type: "Vegetable" },
+    { name: "Bell pepper", type: "Vegetable" },
+    { name: "Onion", type: "Vegetable" },
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <FilteredList items={produce} />
+        </div>
+    );
 }
 
 export default App;
